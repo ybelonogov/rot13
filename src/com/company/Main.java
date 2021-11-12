20c9d1bdf6696d974fda957a55a2dfa100d94cc0
ybelonogov <aleshka59000@gmail.com>
2021-11-12 12:48:23 +0300

    I do it

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -16,34 +16,6 @@ public class Main {
 
        final HashMap<String, String> CONFIG = Reader.readConfig(args[0]);
         Reader.read(CONFIG);
-//        String data = "";
-//        System.out.println(CONFIG);
-//        FileReader reader = new FileReader(args[0]);
-//        Throwable var4 = null;
-//        try {
-//            int c;
-//            try {
-//                while((c = reader.read()) != -1) {
-//                    data = data.concat(String.valueOf((char)c));
-//                }
-//            } catch (Throwable var13) {
-//                var4 = var13;
-//                throw var13;
-//            }
-//        } finally {
-//            if (reader != null) {
-//                if (var4 != null) {
-//                    try {
-//                        reader.close();
-//                    } catch (Throwable var12) {
-//                        var4.addSuppressed(var12);
-//                    }
-//                } else {
-//                    reader.close();
-//                }
-//            }
-//
-//        }
-//
+
     }
 }
--- src/com/company/Reader.java
+++ src/com/company/Reader.java
@@ -1,7 +1,9 @@
 package com.company;
 
 import java.io.*;
+import java.math.BigInteger;
 import java.util.Arrays;
+import java.util.BitSet;
 import java.util.HashMap;
 import java.util.List;
 
@@ -33,10 +35,25 @@ public class Reader {
         try (FileInputStream fin = new FileInputStream(CONFIG.get("input"));
              FileOutputStream fos = new FileOutputStream(CONFIG.get("output"), false)) {
 //            byte[] buffer = new byte[Integer.parseInt(CONFIG.get("bufferSize"))];
-            while (fin.available() !=0) {
+            while (fin.available() != 0) {
                 byte[] buffer = new byte[Integer.parseInt(CONFIG.get("bufferSize"))];
-                fin.read(buffer, 0, Math.min(buffer.length,fin.available()));
+                fin.read(buffer, 0, Math.min(buffer.length, fin.available()));
+                System.out.println();
+                System.out.println();
+                for (byte b : buffer) {
+                    System.out.print(String.format("%8s", Integer.toBinaryString(b & 0xFF)).replace(' ', '0'));
+                }
+//                System.out.println();
                 fos.write(rotate(CONFIG,buffer), 0, buffer.length);
+//            реализация 2
+                BitSet data = BitSet.valueOf(buffer);
+//                fos.write(rotate(CONFIG, data).toByteArray(), 0, buffer.length);
+
+                System.out.println();
+//                for (byte b : rotate(CONFIG, data).toByteArray()) {
+////                    System.out.print(String.format("%x", b));
+//                    System.out.print(String.format("%8s", Integer.toBinaryString(b & 0xFF)).replace(' ', '0'));
+//                }
             }
 //            if (fin.available() >0){
 //                byte[] buffer = new byte[Integer.parseInt(CONFIG.get("bufferSize"))];
@@ -50,19 +67,40 @@ public class Reader {
             System.out.println(ex.getMessage());
         }
     }
-    static byte[] rotate(final HashMap<String, String> CONFIG,byte[] data) {
 
+
+
+    static byte[] rotate(final HashMap<String, String> CONFIG, byte[] data) throws IOException {
+        int len = Integer.parseInt(CONFIG.get("bufferSize")) * 8;
+        int shift = Integer.parseInt(CONFIG.get("shiftSize")) % len;
+        System.out.println();
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         byte[] result = new byte[Integer.parseInt(CONFIG.get("bufferSize"))];
-//        List<Byte> result_list=Arrays.asList(result);
-        int shift = Integer.parseInt(CONFIG.get("shiftSize")) % Integer.parseInt(CONFIG.get("bufferSize"));
-        byte[] result2 = Arrays.copyOfRange(data, 0, shift);
-        byte[] result1 = Arrays.copyOfRange(data, shift, Integer.parseInt(CONFIG.get("bufferSize")));
-        for (int i = 0; i < Integer.parseInt(CONFIG.get("bufferSize")); i++) {
-            if (i < Integer.parseInt(CONFIG.get("bufferSize")) - shift)
-                result[i] = result1[i];
-            else
-                result[i] = result2[i - (Integer.parseInt(CONFIG.get("bufferSize")) - shift)];
+        shift=shift+len;
+        for (int i=0;i<len;i++){
+            int num=i/8;
+            int bit=7-(i)%8;
+            int newBit=(i + shift) % len;
+            int newNum=newBit/8;
+            newBit=7-newBit%8;
+            if(get(data[num],bit)){
+                result[newNum]|= 1<<newBit;
+            }
+//            for (byte b : result) {
+//                System.out.print(String.format("%8s", Integer.toBinaryString(b & 0xFF)).replace(' ', '0'));
+//            }
+//            System.out.println();
         }
-    return result;
+        for (byte b : result) {
+            System.out.print(String.format("%8s", Integer.toBinaryString(b & 0xFF)).replace(' ', '0'));
+        }
+        return result;
+    }
+    static boolean get(byte b,int pos){
+        return (b & (1 << pos)) != 0;
+    }
+    static void set(byte b,int pos){
+        b |= 1 << pos;
+
     }
 }
