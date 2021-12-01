8fe1207ea6207346733015fb39567ded7a23fb25
ybelonogov <aleshka59000@gmail.com>
2021-12-01 21:12:51 +0300

    I do it 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,21 +1,45 @@
 package com.company;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 //import java.io.Reader;
 import java.util.HashMap;
 
 public class Main {
+
     public Main() {
     }
 
-    public static void main(String[] args) throws IOException {
-        if (args.length > 1) {
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Wrong config name format ");
             System.exit(1);
         }
 
-       final HashMap<String, String> CONFIG = Reader.readConfig(args[0]);
-        Reader.read(CONFIG);
-
+        try {
+            final HashMap<String, String> CONFIG = Reader.readConfig(args[0]);
+            try {
+                Reader.read(CONFIG);
+            } catch (NullPointerException | NumberFormatException e) {
+                System.out.println("Wrong config, rule of config: output:='выходной фаил' ");
+                System.out.println(" input:='входной фаил' ");
+                System.out.println(" shiftSize:='размер сдвига' ");
+                System.out.println(" bufferSize:='размер буфера' ");
+                System.exit(1);
+            } catch (FileNotFoundException e) {
+                System.out.println("wrong input file name");
+                System.exit(1);
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Wrong buffer size, rule of config: bufferSize:='NUMBER bytes in buffer' ");
+            System.exit(1);
+        }catch (FileNotFoundException e) {
+            System.out.println("wrong config file name");
+            System.exit(1);
+        }catch (IOException e) {
+            System.out.println("что то с конфигом,input error ");
+            System.exit(1);
+        }
     }
 }
--- src/com/company/Reader.java
+++ src/com/company/Reader.java
@@ -8,99 +8,80 @@ import java.util.HashMap;
 import java.util.List;
 
 public class Reader {
+    final static String IN="input";
+    final static String OUT="output";
+    final static String BUFFER="bufferSize";
+    final static String SHIFT="shiftSize";
+    final static String SPLIT_CHAR=":=";
 
-    static final HashMap<String, String> readConfig(String configFile) throws IOException {
+
+    static HashMap<String, String> readConfig(String configFile) throws IOException {
         HashMap<String, String> result = new HashMap<String, String>();
-        try (FileReader reader = new FileReader(configFile)) {
-            BufferedReader bReader = new BufferedReader(reader);
-            String line = bReader.readLine();
-            int flag = 1;
-            while (line != null) {
-
-                line = line.trim();
-                String[] words = line.split(":=");
-                if (words.length == 2)
-                    result.put(words[0], words[1]);
-                line = bReader.readLine();
-            }
-            bReader.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+        FileReader reader = new FileReader(configFile);
+        BufferedReader bReader = new BufferedReader(reader);
+        String line = bReader.readLine();
+        while (line != null) {
+            line = line.trim();
+            String[] words = line.split(SPLIT_CHAR);
+            if (words.length == 2)
+                result.put(words[0], words[1]);
+            line = bReader.readLine();
+        }
+        bReader.close();
+        if (Integer.parseInt(result.get(BUFFER))<=0) {
+            System.out.println("buffer size < 1");
             System.exit(1);
         }
         return result;
     }
 
-    static void read(final HashMap<String, String> CONFIG) throws FileNotFoundException {
-        try (FileInputStream fin = new FileInputStream(CONFIG.get("input"));
-             FileOutputStream fos = new FileOutputStream(CONFIG.get("output"), false)) {
-//            byte[] buffer = new byte[Integer.parseInt(CONFIG.get("bufferSize"))];
+    static void read(final HashMap<String, String> CONFIG) throws IOException {
+        try (FileInputStream fin = new FileInputStream(CONFIG.get(IN));
+             FileOutputStream fos = new FileOutputStream(CONFIG.get(OUT), false)) {
             while (fin.available() != 0) {
-                byte[] buffer = new byte[Integer.parseInt(CONFIG.get("bufferSize"))];
+                byte[] buffer = new byte[Integer.parseInt(CONFIG.get(BUFFER))];
                 fin.read(buffer, 0, Math.min(buffer.length, fin.available()));
-                System.out.println();
-                System.out.println();
-                for (byte b : buffer) {
-                    System.out.print(String.format("%8s", Integer.toBinaryString(b & 0xFF)).replace(' ', '0'));
-                }
 //                System.out.println();
-                fos.write(rotate(CONFIG,buffer), 0, buffer.length);
-//            реализация 2
-                BitSet data = BitSet.valueOf(buffer);
-//                fos.write(rotate(CONFIG, data).toByteArray(), 0, buffer.length);
-
-                System.out.println();
-//                for (byte b : rotate(CONFIG, data).toByteArray()) {
-////                    System.out.print(String.format("%x", b));
+//                System.out.println();
+//                for (byte b : buffer) {
 //                    System.out.print(String.format("%8s", Integer.toBinaryString(b & 0xFF)).replace(' ', '0'));
 //                }
+                fos.write(rotate(CONFIG, buffer), 0, buffer.length);
+//                System.out.println();
             }
-//            if (fin.available() >0){
-//                byte[] buffer = new byte[Integer.parseInt(CONFIG.get("bufferSize"))];
-//                fin.read(buffer, 0, fin.available());
-//                System.out.println(Arrays.toString(buffer));
-//                fos.write(buffer, 0, buffer.length);
-//            }
 
-        } catch (IOException ex) {
-
-            System.out.println(ex.getMessage());
         }
     }
-
 
 
     static byte[] rotate(final HashMap<String, String> CONFIG, byte[] data) throws IOException {
-        int len = Integer.parseInt(CONFIG.get("bufferSize")) * 8;
-        int shift = Integer.parseInt(CONFIG.get("shiftSize")) % len;
+        int len = Integer.parseInt(CONFIG.get(BUFFER)) * 8;
+        int shift = Integer.parseInt(CONFIG.get(SHIFT)) % len;
+
         System.out.println();
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        byte[] result = new byte[Integer.parseInt(CONFIG.get("bufferSize"))];
-        shift=shift+len;
-        for (int i=0;i<len;i++){
-            int num=i/8;
-            int bit=7-(i)%8;
-            int newBit=(i + shift) % len;
-            int newNum=newBit/8;
-            newBit=7-newBit%8;
-            if(get(data[num],bit)){
-                result[newNum]|= 1<<newBit;
+        byte[] result = new byte[Integer.parseInt(CONFIG.get(BUFFER))];
+        shift = shift + len;
+        for (int i = 0; i < len; i++) {
+            int num = i / 8;
+            int bit = 7 - (i) % 8;
+            int newBit = (i + shift) % len;
+            int newNum = newBit / 8;
+            newBit = 7 - newBit % 8;
+            if (get(data[num], bit)) {
+                result[newNum] |= 1 << newBit;
             }
-//            for (byte b : result) {
-//                System.out.print(String.format("%8s", Integer.toBinaryString(b & 0xFF)).replace(' ', '0'));
-//            }
-//            System.out.println();
         }
-        for (byte b : result) {
-            System.out.print(String.format("%8s", Integer.toBinaryString(b & 0xFF)).replace(' ', '0'));
-        }
+//        for (byte b : result) {
+//            System.out.print(String.format("%8s", Integer.toBinaryString(b & 0xFF)).replace(' ', '0'));
+//        }
         return result;
     }
-    static boolean get(byte b,int pos){
+
+    static boolean get(byte b, int pos) {
         return (b & (1 << pos)) != 0;
     }
-    static void set(byte b,int pos){
-        b |= 1 << pos;
-
-    }
+//
+//    static void set(byte b, int pos) {
+//        b |= 1 << pos;
+//    }
 }
